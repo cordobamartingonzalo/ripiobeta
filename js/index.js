b98818e5302f249eafe85cc7de60f46edecffe84
cordobamartingonzalo <52433230+cordobamartingonzalo@users.noreply.github.com>
2022-05-06 11:27:34 -0300

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,25 @@
+window.sr = ScrollReveal();
+
+    sr.reveal ('.textos-header', {
+        duration: 3000,
+        origin: 'bottom',
+        distance: '-100px'
+    })
+
+    sr.reveal ('.contenedor-sobre-nosotros', {
+        duration: 3000,
+        origin: 'left',
+        distance: '-200px'
+    })
+
+    sr.reveal ('.cards', {
+        duration: 3000,
+        origin: 'right',
+        distance: '-100px'
+    })
+
+    sr.reveal ('.servicio-cont', {
+        duration: 3000,
+        origin: 'top',
+        distance: '-100px'
+    })
